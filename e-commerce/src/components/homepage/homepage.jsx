01ac5f140d83236665ad9a5710e2679abfe54eb7
dbpatnodee <dbpatnode@hotmail.com>
2021-06-10 11:39:13 -0700

    setting up inital page outlay

---
+++ e-commerce/src/components/homepage/homepage.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menue">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">hats</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+            <div className="directory-menue">
+                <div className="menu-item">
+                    <h1 className="title">jackets</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+            <div className="directory-menue">
+                <div className="menu-item">
+                    <h1 className="title">shoes</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+            <div className="directory-menue">
+                <div className="menu-item">
+                    <h1 className="title">mens</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+            <div className="directory-menue">
+                <div className="menu-item">
+                    <h1 className="title">womens</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
