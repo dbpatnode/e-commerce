d7c25ccbf7b189a8cc185fa6685be4a086588d36
dbpatnodee <dbpatnode@hotmail.com>
2021-06-21 14:52:56 -0700

    overflow hidden keeps transition within div

--- e-commerce/src/components/menu-item/menu-item.jsx
+++ e-commerce/src/components/menu-item/menu-item.jsx
@@ -4,9 +4,11 @@ import './menu-item.scss'
 const MenuItem = ({ title, imageUrl }) => (
     <div className="menu-item">
         <div
-            className="background-image" style={{
+            className="background-image"
+            style={{
                 backgroundImage: `url(${imageUrl})`
-            }} />
+            }}
+        />
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
