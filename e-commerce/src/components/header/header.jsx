aa18fca5ee763f5d897c02f843b3dfbf15841329
dbpatnodee <dbpatnode@hotmail.com>
2021-07-01 14:00:08 -0700

    logo showing up

--- e-commerce/src/components/header/header.jsx
+++ e-commerce/src/components/header/header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.scss';
-// import { ReactComponent as Logo } from '../../assets/cube.png'
+import Logo from '../../assets/cube.png'
 
 const Header = () => (
     <div className="header">
         <Link to="/" className="logo-container">
-            <img src="./cube.png" alt="logo"></img>
+            <img src={Logo} alt="logo"></img>
         </Link>
 
         <div className="options">
