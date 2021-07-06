10b1fdd432f3c1a4b6a4680a9d7f21362f85b5a5
dbpatnodee <dbpatnode@hotmail.com>
2021-07-06 14:58:28 -0700

    forms set up

--- e-commerce/src/components/sign-in/sign-in.jsx
+++ e-commerce/src/components/sign-in/sign-in.jsx
@@ -12,6 +12,13 @@ class SignIn extends React.Component {
         this.setState({ email: '', password: '' })
     }
 
+    // this is grabbing whatever the name value is from either input (in this case email and password)
+    // and assigning it the value of whatever is typed
+    handleChange = (e) => {
+        const { value, name } = e.target
+        this.setState({ [name]: value })
+    }
+
     render() {
         return (
             <div className="sign-in">
@@ -19,10 +26,20 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password.</span>
 
                 <form onSubmit={(e) => this.handleSubmit}>
-                    <input name="email" type="email" required />
+                    <input
+                        name="email"
+                        type="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required />
                     <label>Email</label>
 
-                    <input name="password" type="password" required />
+                    <input
+                        name="password"
+                        type="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        required />
                     <label>Password</label>
 
                     <input type="submit" value="Sign In" />
